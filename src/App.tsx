be4751dafce9ea4e31e4c3894564605f6c4594cc
UmmeZaiba-i <ummez@ivoyant.com>
2024-07-29 11:33:42 +0530

    Update

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Cart from './components/Cart'
+import ProductsLists from './components/ProductsLists'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      <ProductsLists/>
+      <Cart/>
+    </div>
+  )
+}
+
+export default App
